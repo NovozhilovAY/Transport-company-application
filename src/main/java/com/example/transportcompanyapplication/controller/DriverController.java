552ca68235587d81c448c5d788e36803ded06a4c
NovozhilovAY <sasha.n191@yandex.ru>
2022-02-09 14:56:17 +0300

    add DriverService

--- src/main/java/com/example/transportcompanyapplication/controller/DriverController.java
+++ src/main/java/com/example/transportcompanyapplication/controller/DriverController.java
@@ -2,7 +2,7 @@ package com.example.transportcompanyapplication.controller;
 
 import com.example.transportcompanyapplication.exceptions.ResourceNotFoundException;
 import com.example.transportcompanyapplication.model.Driver;
-import com.example.transportcompanyapplication.repository.DriverRepository;
+import com.example.transportcompanyapplication.service.DriverService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -12,42 +12,34 @@ import java.util.List;
 @RequestMapping("/api/drivers")
 public class DriverController {
 
-    final private DriverRepository repository;
+    final private DriverService service;
 
-    public DriverController(DriverRepository repository) {
-        this.repository = repository;
+    public DriverController(DriverService service) {
+        this.service = service;
     }
 
     @GetMapping
     public List<Driver> getAllDrivers() {
-        return repository.findAll();
+        return service.findAll();
     }
 
     @GetMapping("/{id}")
     public Driver getDriverById(@PathVariable(value = "id") Long id) throws ResourceNotFoundException {
-        return repository.findById(id).orElseThrow(
-                ()->new ResourceNotFoundException("Driver with id = " + id + " not found"));
+        return service.findById(id);
     }
 
     @PostMapping
     public Driver createDriver(@RequestBody @Valid Driver driver){
-        return repository.save(driver);
+        return service.save(driver);
     }
 
     @DeleteMapping("/{id}")
     public Driver deleteDriver(@PathVariable(value = "id") Long id) throws ResourceNotFoundException {
-        Driver driver = repository.findById(id).orElseThrow(
-                ()->new ResourceNotFoundException("Driver with id = " + id + " not found")
-        );
-        repository.delete(driver);
-        return driver;
+        return service.delete(id);
     }
 
     @PutMapping
     public Driver updateDriver(@RequestBody @Valid Driver driver) throws ResourceNotFoundException {
-        repository.findById(driver.getId()).orElseThrow(
-                ()-> new ResourceNotFoundException("Driver with id = " + driver.getId() + " not found")
-        );
-        return repository.save(driver);
+        return service.update(driver);
     }
 }
---
+++ src/main/java/com/example/transportcompanyapplication/service/DriverService.java
@@ -0,0 +1,43 @@
+package com.example.transportcompanyapplication.service;
+
+import com.example.transportcompanyapplication.exceptions.ResourceNotFoundException;
+import com.example.transportcompanyapplication.model.Driver;
+import com.example.transportcompanyapplication.repository.DriverRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DriverService {
+    private final DriverRepository repository;
+
+    public DriverService(DriverRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Driver> findAll() {
+        return repository.findAll();
+    }
+
+    public Driver findById(Long id) throws ResourceNotFoundException {
+        return repository.findById(id).orElseThrow(
+                ()->new ResourceNotFoundException("Driver with id = " + id + " not found"));
+    }
+
+    public Driver save(Driver driver) {
+        return repository.save(driver);
+    }
+
+    public Driver delete(Long id) throws ResourceNotFoundException {
+        Driver deletedDriver = findById(id);
+        repository.delete(deletedDriver);
+        return deletedDriver;
+    }
+
+    public Driver update(Driver driver) throws ResourceNotFoundException {
+        findById(driver.getId());
+        return repository.save(driver);
+    }
+
+
+}
