84836714a7dd39d34be54de8e4253a0a67a4ea58
NovozhilovAY <sasha.n191@yandex.ru>
2022-02-22 23:22:01 +0300

    update RepositoryConfig

--- src/main/java/com/example/transportcompanyapplication/repository/extended/RepositoryConfig.java
+++ src/main/java/com/example/transportcompanyapplication/repository/extended/RepositoryConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(repositoryBaseClass = ExtendedRepositoryImpl.class)
+@EnableJpaRepositories(basePackages = {"com.example.transportcompanyapplication.repository"},
+        repositoryBaseClass = ExtendedRepositoryImpl.class)
 public class RepositoryConfig {
 }
